3f6d9ded0d20215bc3054098f08bfd88a6fac611
ianiki11 <ianiki10@yandex.ru>
2021-08-21 14:11:05 +0300

    calculator for task 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.io.*;
+import lab_1.task_1.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //new text bla bla bla
+        Calculator calc = new Calculator();
+        System.out.println("Введите первое число: ");
+        calc.setA();
+        System.out.println("Введите второе число: ");
+        calc.setB();
+        System.out.println("Остаток деления первого на второе: " + calc.getReminder(calc.getA(), calc.getB()));
+        
     }
 }
--- src/lab_1/task_1/Calculator.java
+++ src/lab_1/task_1/Calculator.java
@@ -1,7 +1,52 @@
 package lab_1.task_1;
 
+import java.io.*;
+
 public class Calculator {
+
+    private int a;
+    private int b;
+    private int c;
+    BufferedReader input =  new BufferedReader(new InputStreamReader(System.in));
+
+    public void setA() {
+        try {
+            a = Integer.parseInt(input.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setB() {
+        try {
+            b = Integer.parseInt(input.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public int getB() {
+        return b;
+    }
+
+    public void setC() {
+        try {
+            c = Integer.parseInt(input.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public int getC() {
+        return c;
+    }
+
+
     public int getReminder(int a, int b) {
         return a % b;
     }
+
+
 }
